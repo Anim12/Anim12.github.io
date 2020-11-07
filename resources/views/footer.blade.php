e87665aff3f799f107220b31958c917997e0ca07
Gangashri <gangashri@gangashri.domain.name>
2020-11-07 19:22:21 +0530

    first project

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,24 @@
+<style>
+.footer{
+
+  display: block;
+  display: flex;
+  justify-content: center;
+  color:#000000;
+  height: 50px;
+  background-color: #434b4f;
+  border: 1px solid #636b6f;
+  height: relative; /* height + top/bottom paddding + top/bottom border must add up to footer height */
+  width: 100%;
+  position: relative;
+  top:50px;
+  bottom: 0px;
+
+}
+</style>
+
+<footer class="footer">
+
+  <span>&copy;2020<strong>Gangagshri Computers</strong>|Developed and Designed by <strong>Animesh</strong>.|Contact-+91-9767757349|Email:gangashribsp@gmail.com</span>
+
+</footer>
